74866c9ef71711c79e65f5b7332f16fd9ce4a6a5
Priyanshi-Pandey20 <pandeypriyanshi2005@gmail.com>
2026-03-10 12:58:17 +0530

    fix enviornment url

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,7 +1,7 @@
 let IS_PROD = true;
 
 const server = IS_PROD
-  ? "https://zoomclonebackend-k4gt.onrender.com/"
-  : "http/localhost:8000";
+  ? "https://zoomclonebackend-k4gt.onrender.com"
+  : "http:/localhost:8000";
 
 export default server;
